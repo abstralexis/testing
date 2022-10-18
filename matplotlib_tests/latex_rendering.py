f75327ddbfe17380d12e12f841091a2d28fae281
AlexisComix <thecomixguy@hotmail.com>
2022-10-18 19:13:13 +0100

    Make the text actually draw

--- matplotlib_tests/latex_rendering.py
+++ matplotlib_tests/latex_rendering.py
@@ -1,4 +1,4 @@
-# This is a test program I got from Stack Overflow and added
+# This is a test program I got from Stack Overflow, tweaked, and added
 # comments for rendering LaTeX to the screen. I plan to make a discord
 # bot eventually that returns a rendered image of maths stuff
 # such that a user can pass a LaTeX syntax string as an argument 
@@ -12,9 +12,14 @@ rcParams['text.usetex'] = True  # Set it so that you can use LaTeX rendering
 
 plt.style.use('dark_background')    # Dark mode !!!
 
-latex_expression = r"$\frac{1}{2}$" # Some LaTeX expr that you can pass
+latex_expression = r"""
+The point $(h, k)$ lies on $f(x)$.
+Any transformation of $f(x)$ in the form
+$af(bx + c) + d$ results in the transformation
+of the point $(h, k)$ to $(\frac{h - c}{b}, ak + d)$.  
+""" # Some LaTeX expr that you can pass
 
-plt.text(0.0, 0.0, latex_expression, fontsize=14)   # Render the LaTeX text
+plt.text(0.01, 0.5, latex_expression, fontsize=18)   # Render the LaTeX text
 
 # Axes stuff
 ax = plt.gca()  # Get the state of the current axes
@@ -23,4 +28,9 @@ ax.axes.get_xaxis().set_visible(False)
 ax.axes.get_yaxis().set_visible(False)
 
 # Show the plot
+plt.draw()
 plt.show()
+
+# I think that in the final program, I will have to do some fancy maths
+# to figure out what the font size should be and also text wrapping and such.
+# Maybe also editing the image from matplotlib with pillow to look nicer.
