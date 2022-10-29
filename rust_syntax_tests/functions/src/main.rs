2d704a2f86afa7bdcdf6a1d6b5340353e0fa6536
AlexisComix <Thecomixguy@hotmail.com>
2022-10-29 22:57:28 +0100

    Add some basic Rust syntax examples

---
+++ rust_syntax_tests/functions/src/main.rs
@@ -0,0 +1,39 @@
+/*
+This is to explore how functions work in Rust with some basic
+examples
+*/
+
+fn main() {
+    println!("Hello, world!");
+    some_function();                        // Call a function
+
+    let some_str = String::from("Foo");     // Make a variable
+    fn_with_args(some_str);                 // Pass the variable in!
+
+    let num: i32 = 10;                      // Make a variable
+    let doubled: i32 = fn_with_return(num); // Assign output to new variable!
+    println!("{}", doubled);                // Print the output!
+
+    let mut bar: i32 = 16;                  // Here is a mutable variable!
+    let mut baz: i32 = 25;                  // And another!
+    borrower(&bar, &mut baz);               // Borrow them with "&"!
+    println!("bar: {} baz: {}", bar, baz);  // Print the values
+}
+
+fn some_function() {
+    println!("This is some_function!");     // This is a function!
+}
+
+fn fn_with_args(s: String) {                // Take in an argument
+    println!("{}", s);                      // Print the argument!
+}
+
+fn fn_with_return(x: i32) -> i32 {          // This function returns an i32!
+    let two_times_x: i32 = 2 * x;           // Double the value
+    return two_times_x;                     // Return the value!
+}
+
+fn borrower(y: &i32, z: &mut i32) {         // Borrow the values!
+    // y = 42;                              // Fails, is read-only
+    *z = 29;                                // Dereference z to assign!
+}
---
+++ rust_syntax_tests/variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    /*
+    This is for demonstrating syntax for declaring variables
+    and using them in basic ways.
+    */
+    let immutable_number: i32 = 0;      // Rust variables are immutable by default.
+    // immutable_number = 1;            // This would not compile.
+    let mut mutable_number: i32 = 5;    // Set a mutable variable with "mut"
+    mutable_number = 10;                // Change the mutable variable
+    println!("immutable: {} mutable: {}", immutable_number, mutable_number);
+}
