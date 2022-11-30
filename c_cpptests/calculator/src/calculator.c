edcc84c617ab95ab1ac0c1516a2fe771ac7ffe4d
AlexisComix <Thecomixguy@hotmail.com>
2022-11-30 19:25:43 +0000

    Implement the basic calculation functions

--- c_cpptests/calculator/src/calculator.c
+++ c_cpptests/calculator/src/calculator.c
@@ -7,17 +7,53 @@ This is my attempt at making a basic calculator in C
 #include "calculator.h"
 
 int main(int argc, char **argv) {
-    printf("Hello World!\n");
+    /*
+    Main function
+    */
 
+    // Declare variables    
     int a = 10;
     int b = 100;
-    int o = add(a, b);
 
-    printf("The output is: %d\n", o);
+    // Get outputs from mathematical functions
+    int output1 = add(a, b);
+    int output2 = subtract(a, b);
+    int output3 = multiply(a, b);
+    int output4 = divide(b, a);
+
+    // Print the outputs
+    printf("The output is: %d\n", output1);
+    printf("The output is: %d\n", output2);
+    printf("The output is: %d\n", output3);
+    printf("The output is: %d\n", output4);
 
     return 0;
 }
 
 int add(int a, int b) {
+    /*
+    Adds two numbers, a and b
+    */
     return a + b;
+}
+
+int subtract(int a, int b) {
+    /*
+    Subtracts b from a
+    */
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    /*
+    Multiplies two numbers, a and b
+    */
+    return a * b;
+}
+
+int divide(int a, int b) {
+    /*
+    Divides a by b
+    */
+    return a / b;
 }
