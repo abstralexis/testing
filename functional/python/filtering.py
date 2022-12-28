e391905c376907d29b6a643946f623e73ed5aa4f
AlexisComix <Thecomixguy@hotmail.com>
2022-12-28 22:16:04 +0000

    Add comments

--- functional/python/filtering.py
+++ functional/python/filtering.py
@@ -2,7 +2,14 @@
 This illustrates the concept of a filter.
 """
 
+# Make a list from a range object
 some_list = list(range(0, 100))
+
+# Filter with a lambda function that returns a bool
+# The item gets filtered out if the anonymous function 
+# returns False.
 evens = list(filter(lambda x: x % 2 == 0, some_list))
+
+# Print
 print(some_list)
 print(evens)
