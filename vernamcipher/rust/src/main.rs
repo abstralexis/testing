de960fdfa84099c8040f92bd77a77a61a1b94644
AlexisComix <thecomixguy@hotmail.com>
2023-01-17 16:34:00 +0000

    Use <String>.into_bytes() and change some types

--- vernamcipher/rust/src/main.rs
+++ vernamcipher/rust/src/main.rs
@@ -4,15 +4,15 @@
  */
 
 fn main() {
-    let text: &str = "1234567890";
-    let key: &str = "qwertyuiop";
+    let text: String = "1234567890".to_owned();
+    let key: String = "qwertyuiop".to_owned();
     
     // Convert to a u8 representation
-    let text_bin: Vec<u32> = to_binary(text);
-    let key_bin: Vec<u32> = to_binary(key);
+    let text_bin: Vec<u8> = to_binary(text);
+    let key_bin: Vec<u8> = to_binary(key);
 
     // Xor the key and text
-    let text_xor_key: Vec<u32> = binary_xor(text_bin, key_bin);
+    let text_xor_key: Vec<u8> = binary_xor(text_bin, key_bin);
 
     // Convert back to string
     let ciphertext: String = to_string(text_xor_key);
@@ -25,30 +25,26 @@ fn main() {
     println!("{}", ciphertext);
 }
 
-fn to_binary(plaintext: &str) -> Vec<u32> {
+fn to_binary(plaintext: String) -> Vec<u8> {
     /*
-        Returns a Vec of u32s that represent the binary 
+        Returns a Vec of u8s that represent the binary 
         representation of every char in plaintext.
      */
-    let mut binary_vec: Vec<u32> = Vec::new();
-    for c in plaintext.chars() {
-        binary_vec.push(c as u32);
-    }
-    return binary_vec;
+    plaintext.into_bytes()
 }
 
-fn binary_xor(bin_text: Vec<u32>, bin_key: Vec<u32>) -> Vec<u32> {
+fn binary_xor(bin_text: Vec<u8>, bin_key: Vec<u8>) -> Vec<u8> {
     bin_text
         .iter()
         .zip(bin_key.iter())
         .map(|(&a, &b)| a ^ b)
-        .collect::<Vec<u32>>()
+        .collect::<Vec<u8>>()
 }
 
-fn to_string(bin_text: Vec<u32>) -> String {
+fn to_string(bin_text: Vec<u8>) -> String {
     bin_text
         .iter()
-        .map(|x| match std::char::from_u32(*x) {
+        .map(|x| match std::char::from_u32((*x).into()) {
             Some(c) => c,
             None => '�',
         })
