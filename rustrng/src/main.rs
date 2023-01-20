055586f75eecbcdcc85f1bc1a85c0639bdf16824
AlexisComix <thecomixguy@hotmail.com>
2023-01-20 17:38:09 +0000

    Add random key generation to Vernam cipher

---
+++ rustrng/src/main.rs
@@ -0,0 +1,17 @@
+use rand::prelude::*;
+
+fn main() {
+    println!("{:?}", gen_ascii_chars(64));
+}
+
+fn gen_ascii_chars(length: u32) -> String {
+    let mut arr: Vec<u32> = (0..length)
+        .collect::<Vec<u32>>()
+        .into_iter()
+        .map(|_| thread_rng().gen())
+        .collect::<Vec<u32>>();
+    arr
+        .into_iter()
+        .map(|x| x.to_string())
+        .collect::<String>()
+}
--- vernamcipher/rust/src/main.rs
+++ vernamcipher/rust/src/main.rs
@@ -3,12 +3,13 @@
     cipher implemented in Rust.
  */
 
+ use rand::prelude::*;
+
 fn main() {
     let text: String = "asigfoegofbioiba82u10u".to_owned();
-    let key: String = vec![0; text.len()]
-        .into_iter()
-        .map(|x| x.to_string())
-        .collect::<String>();
+    let key: String = gen_ascii_chars(
+        text.len().try_into().unwrap()
+    );
     
     // Convert to a u8 representation
     let text_bin: Vec<u8> = to_binary(text);
@@ -18,7 +19,7 @@ fn main() {
     let text_xor_key: Vec<u8> = binary_xor(text_bin, key_bin);
 
     // Convert back to string
-    let ciphertext: String = to_string(text_xor_key);
+    let ciphertext: String = vecu8_to_string(text_xor_key);
 
     // Print the text such that it shows unicode sequences
     // That it cannot represent
@@ -26,6 +27,15 @@ fn main() {
 
     // Print what it can represent
     println!("{}", ciphertext);
+}
+
+fn gen_ascii_chars(length: u8) -> String {
+    let arr: Vec<u8> = (0..length)
+        .collect::<Vec<u8>>()
+        .into_iter()
+        .map(|_| thread_rng().gen())
+        .collect::<Vec<u8>>();
+    vecu8_to_string(arr)
 }
 
 fn to_binary(plaintext: String) -> Vec<u8> {
@@ -44,7 +54,7 @@ fn binary_xor(bin_text: Vec<u8>, bin_key: Vec<u8>) -> Vec<u8> {
         .collect::<Vec<u8>>()
 }
 
-fn to_string(bin_text: Vec<u8>) -> String {
+fn vecu8_to_string(bin_text: Vec<u8>) -> String {
     bin_text
         .iter()
         .map(|x| match std::char::from_u32((*x).into()) {
