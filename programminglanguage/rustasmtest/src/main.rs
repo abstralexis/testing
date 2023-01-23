79182dab4545c204a7b545f5153ab14b0129bab6
AlexisComix <Thecomixguy@hotmail.com>
2023-01-23 19:49:37 +0000

    rustfmt

--- programminglanguage/rustasmtest/src/main.rs
+++ programminglanguage/rustasmtest/src/main.rs
@@ -1,23 +1,22 @@
-use std::{env, fs, collections::HashMap};
+use std::{collections::HashMap, env, fs};
 
 // Static array used for checking if a token is a keyword
-static KEYWORDS: &'static [&'static str] = &[
-    "iadd",
-    "out",
-];
+static KEYWORDS: &'static [&'static str] = &["iadd", "out"];
 
-fn main(){
-    let args: Vec<String> = env::args().collect();  // Collect args passed on run
-    if args.len() != 2 {panic!("Invalid arguments")}// Panic on invalid no. args
-    let filepath: &String = &args[1];               // Get filepath
+fn main() {
+    let args: Vec<String> = env::args().collect(); // Collect args passed on run
+    if args.len() != 2 {
+        panic!("Invalid arguments")
+    } // Panic on invalid no. args
+    let filepath: &String = &args[1]; // Get filepath
 
     let mut memory: HashMap<String, i32> = HashMap::new();
     let lines: Vec<String> = read_lines(&filepath);
     let tokenised_lines: Vec<Vec<String>> = tokenise_lines(&lines);
 
     // dbg!(
-    //     &args, 
-    //     &filepath, 
+    //     &args,
+    //     &filepath,
     //     &lines,
     //     &tokenised_lines,
     //     &memory,
@@ -36,14 +35,19 @@ fn run_line(line: &Vec<String>, mem: &mut HashMap<String, i32>) -> () {
     */
     let first: &str = &line[0][..];
 
-    if !KEYWORDS.contains(&first) { panic!("Invalid keyword") }
+    if !KEYWORDS.contains(&first) {
+        panic!("Invalid keyword")
+    }
 
     if line[0] == "iadd" {
-        let a = line[1].parse::<i32>().unwrap_or_else(|_| get_val(&line[1], mem));
-        let b = line[2].parse::<i32>().unwrap_or_else(|_| get_val(&line[2], mem));
+        let a = line[1]
+            .parse::<i32>()
+            .unwrap_or_else(|_| get_val(&line[1], mem));
+        let b = line[2]
+            .parse::<i32>()
+            .unwrap_or_else(|_| get_val(&line[2], mem));
         let val = iadd(&a, &b);
         istore(&line[3], &val, Box::new(mem).as_mut());
-
     } else if line[0] == "out" {
         out(&line[1].to_owned(), mem);
     }
@@ -52,14 +56,13 @@ fn run_line(line: &Vec<String>, mem: &mut HashMap<String, i32>) -> () {
 fn get_val(var: &String, mem: &mut HashMap<String, i32>) -> i32 {
     match mem.get(var) {
         Some(i) => return *i,
-        None => panic!("No such variable name {}", var)
+        None => panic!("No such variable name {}", var),
     };
-
 }
 
 fn run_lines(lines: &Vec<Vec<String>>, mem: &mut HashMap<String, i32>) -> () {
     /*
-        Runs the lines 
+        Runs the lines
     */
     for line in lines {
         run_line(&line, mem);
@@ -68,7 +71,7 @@ fn run_lines(lines: &Vec<Vec<String>>, mem: &mut HashMap<String, i32>) -> () {
 
 fn out(var: &String, mem: &mut HashMap<String, i32>) -> () {
     /*
-        Prints a character referenced to by a variable name var in memory mem 
+        Prints a character referenced to by a variable name var in memory mem
     */
     match mem.get(var) {
         Some(x) => println!("{:?}", x),
@@ -92,11 +95,7 @@ fn read_lines(filepath: &String) -> Vec<String> {
     fs::read_to_string(&filepath)
         .expect("Failed to read file.")
         .split(";")
-        .map(|x| x
-            .to_owned()
-            .replace("\r\n", "")
-            .trim()
-            .to_owned())
+        .map(|x| x.to_owned().replace("\r\n", "").trim().to_owned())
         .filter(|x| x != "")
         .collect::<Vec<String>>()
 }
@@ -108,10 +107,11 @@ fn tokenise_lines(lines: &Vec<String>) -> Vec<Vec<String>> {
     */
     lines
         .iter()
-        .map(|line| line
-            .split(" ")
-            .map(|x| x.to_owned())
-            .collect::<Vec<String>>())
+        .map(|line| {
+            line.split(" ")
+                .map(|x| x.to_owned())
+                .collect::<Vec<String>>()
+        })
         .collect::<Vec<Vec<String>>>()
 }
 
