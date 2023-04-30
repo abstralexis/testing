f7766e01e8d57e8a6830b6084d192fe54fb56468
AlexisComix <thecomixguy@hotmail.com>
2023-04-30 09:20:52 +0100

    Add `.push()` function and tests

--- rustsets/src/main.rs
+++ rustsets/src/main.rs
@@ -14,6 +14,16 @@ impl<T: Ord + Hash + Clone> Set<T> {
         Set { items: Vec::new() }.remove_duplicates()
     }
 
+    pub fn push(&self, items: &Vec<T>) -> Set<T> {
+        let mut old: Vec<T> = self.get_items();
+        let mut new: Vec<T> = items.clone();
+        old.append(&mut new);
+
+        Set {
+            items: old
+        }.remove_duplicates()
+    }
+
     fn remove_duplicates(&self) -> Set<T> {
         let mut items = self.items.clone();
         items.sort();
@@ -28,6 +38,7 @@ impl<T: Ord + Hash + Clone> Set<T> {
     pub fn union(&self, set: &Set<T>) -> Set<T> {
         let mut items = self.items.clone();
         items.append(&mut set.get_items());
+
         Set { items: items }.remove_duplicates()
     }
 
@@ -52,6 +63,69 @@ impl<T: Ord + Hash + Clone> Set<T> {
 
     pub fn proper_subset(&self, set: &Set<T>) -> bool {
         self.subset(&set) && self != set
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Set;
+
+    #[test]
+    fn union_test() {
+        let set_a: Set<i32> = Set::new().push(&vec![1, 2, 3]);
+        let set_b: Set<i32> = Set::new().push(&vec![3, 4, 5]);
+        let expected_set: Set<i32> = Set::new().push(&vec![1, 2, 3, 4, 5]);
+        assert!(set_a.union(&set_b) == expected_set);
+    }
+
+    #[test]
+    fn intersection_test() {
+        let set_a: Set<i32> = Set::new().push(&vec![1, 2, 3]);
+        let set_b: Set<i32> = Set::new().push(&vec![2, 3, 4]);
+        let expected_set: Set<i32> = Set::new().push(&vec![2, 3]);
+        assert!(set_a.intersect(&set_b) == expected_set);
+    }
+
+    #[test]
+    fn improper_subset_true_proper() {
+        let set_a: Set<i32> = Set::new().push(&vec![1, 2, 3]);
+        let set_b: Set<i32> = Set::new().push(&vec![1, 2, 3, 4, 5]);
+        assert!(set_a.subset(&set_b) == true);
+    }
+
+    #[test]
+    fn improper_subset_true_improper() {
+        let set_a: Set<i32> = Set::new().push(&vec![1, 2, 3]);
+        let set_b: Set<i32> = Set::new().push(&vec![1, 2, 3]);
+        assert!(set_a.subset(&set_b) == true);
+    }
+
+    #[test]
+    fn improper_subset_false() {
+        let set_a: Set<i32> = Set::new().push(&vec![1, 2]);
+        let set_b: Set<i32> = Set::new().push(&vec![3, 4, 5]);
+        assert!(set_a.subset(&set_b) == false)
+    }
+
+    #[test]
+    fn proper_subset_true() {
+        let set_a: Set<i32> = Set::new().push(&vec![1, 2]);
+        let set_b: Set<i32> = Set::new().push(&vec![1, 2, 3]);
+        assert!(set_a.proper_subset(&set_b) == true);
+    }
+
+    #[test]
+    fn proper_subset_false() {
+        let set_a: Set<i32> = Set::new().push(&vec![1, 2]);
+        let set_b: Set<i32> = Set::new().push(&vec![4, 5, 6]);
+        assert!(set_a.proper_subset(&set_b) == false);
+    }
+
+    #[test]
+    fn proper_subset_improper() {
+        let set_a: Set<i32> = Set::new().push(&vec![1, 2, 3]);
+        let set_b: Set<i32> = Set::new().push(&vec![1, 2, 3]);
+        assert!(set_a.proper_subset(&set_b) == false);
     }
 }
 
